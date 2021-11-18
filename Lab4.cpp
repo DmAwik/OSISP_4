cfcf2de6d4a057e637e621ac4d72a4637c4f1085
DmAwik <Awii15@mail.ru>
2021-11-18 15:06:09 +0300

    fourth

---
+++ Lab4.cpp
@@ -0,0 +1,75 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <stdio.h>
+//#include <conio.h>
+//#include <windows.h>
+#include <locale.h>  
+//using namespace std;
+#include "Sorter.hpp"
+
+LPSTR* GetLinesFromFile(LPWSTR filename, LPDWORD linesCount);
+
+int main()
+{
+	std::string fileName;
+    int numberOfThreads = 0;
+    std::cout << "File name:(write 1.txt)\n";
+    std::cin >> fileName;
+    std::cout << "Number of threads:\n";
+    std::cin >> numberOfThreads;
+	DWORD linesCount = 0;
+	wchar_t wcFileName[100] = { 0 };
+	mbstowcs(wcFileName, fileName.c_str(), fileName.length());
+	LPSTR* lines = GetLinesFromFile(wcFileName, &linesCount);
+	Sort(lines, linesCount, numberOfThreads);
+
+	for (int i = 0; i < linesCount; i++)
+	{
+		printf("%s\n", lines[i]);
+	}
+}
+
+LPSTR* GetLinesFromFile(LPWSTR filename, LPDWORD linesCount)
+{
+	HANDLE hFile = CreateFile(filename, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		return NULL;
+	}
+
+	DWORD lpFileSizeHigh = GetFileSize(hFile, NULL);
+	if (lpFileSizeHigh == INVALID_FILE_SIZE)
+	{
+		CloseHandle(hFile);
+		return NULL;
+	}
+
+	LPSTR lpBuffer = (LPSTR)calloc(lpFileSizeHigh + 1, 1);
+	if (!ReadFile(hFile, lpBuffer, lpFileSizeHigh, NULL, NULL))
+	{
+		CloseHandle(hFile);
+		free(lpBuffer);
+		return NULL;
+	}
+	CloseHandle(hFile);
+
+	LPCSTR lpsDelimiters = "\r\n";
+	LPSTR* lpsResult = (LPSTR*)calloc(0, sizeof(LPSTR)), * lpsBuffer;
+	LPSTR lpsContext = 0;
+	DWORD dwLinesCount = 0;
+	LPSTR lpsToken = strtok_s(lpBuffer, lpsDelimiters, &lpsContext);
+
+	while (lpsToken != NULL)
+	{
+		do
+		{
+			lpsBuffer = (LPSTR*)realloc(lpsResult, (dwLinesCount + 1) * sizeof(LPSTR));
+		} while (lpsBuffer == NULL);
+		lpsResult = lpsBuffer;
+		lpsResult[dwLinesCount++] = lpsToken;
+		lpsToken = strtok_s(NULL, lpsDelimiters, &lpsContext);
+	}
+
+	*linesCount = dwLinesCount;
+	return lpsResult;
+}
